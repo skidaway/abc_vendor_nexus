a0f86843ba3ebc6e8382f5263d04b86fa239c60a
Alex Cruz <alex@dirtyunicorns.com>
2018-10-27 16:56:18 +0200

    Introduce our themes vendor

--- configs/abc_phone.mk
+++ configs/abc_phone.mk
@@ -16,6 +16,7 @@ include vendor/nexus/configs/aosp_fixes.mk
 include vendor/nexus/configs/abc_main.mk
 include vendor/nexus/configs/system_additions.mk
 include vendor/nexus/configs/version.mk
+include vendor/themes/common.mk
 
 # Telephony packages
 PRODUCT_PACKAGES += \
