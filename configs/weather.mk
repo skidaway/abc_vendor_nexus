a89816a7b3dd85e7bb5397f2a40c4b085d2f747c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-07 20:27:12 -0200

    Weather: Add default permissions
    Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

--- configs/weather.mk
+++ configs/weather.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
-    vendor/nexus/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/nexus/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/nexus/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
